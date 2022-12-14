a72e432495f22e25de3e36471733da939fa897af
Matthieu Rosinski <korrigan@slash16.org>
2022-12-14 15:58:30 +0100

    fixed some tests using be_false / be_true instead of be false / be true

--- spec/bloombroom/bits/bit_field_spec.rb
+++ spec/bloombroom/bits/bit_field_spec.rb
@@ -7,47 +7,47 @@ describe Bloombroom::BitField do
     bf = Bloombroom::BitField.new(100)
     (0..99).each do |i|
       bf[i].should == 0
-      bf.include?(i).should be_false
+      bf.include?(i).should be false
     end
   end
 
   it "should set and unset" do
     bf = Bloombroom::BitField.new(1000)
     bf[100].should == 0
-    bf.include?(100).should be_false
+    bf.include?(100).should be false
     bf[101].should == 0
-    bf.include?(101).should be_false
+    bf.include?(101).should be false
 
     bf[100] = 1
     bf[100].should == 1
-    bf.include?(100).should be_true
+    bf.include?(100).should be true
     bf[100] = 0
     bf[100].should == 0
-    bf.include?(100).should be_false
+    bf.include?(100).should be false
 
     bf.set(101)
     bf[101].should == 1
-    bf.include?(101).should be_true
+    bf.include?(101).should be true
     bf.unset(101)
     bf[101].should == 0
-    bf.include?(101).should be_false
+    bf.include?(101).should be false
   end
 
   it "should unset" do
     bf = Bloombroom::BitField.new(100)
-    (0..99).each{|i| bf.include?(i).should be_false}
+    (0..99).each{|i| bf.include?(i).should be false}
     (0..31).each{|i| bf.unset(i)}
-    (0..99).each{|i| bf.include?(i).should be_false}
-    
+    (0..99).each{|i| bf.include?(i).should be false}
+
     (0..31).each{|i| bf.set(i)}
-    (0..31).each{|i| bf.include?(i).should be_true}
-    (32..99).each{|i| bf.include?(i).should be_false}
+    (0..31).each{|i| bf.include?(i).should be true}
+    (32..99).each{|i| bf.include?(i).should be false}
 
     unsetbits = [0, 5, 6, 10, 16, 23, 31]
     unsetbits.each{|i| bf.unset(i)}
-    ((0..31).map{|i| i} - unsetbits).each{|i| bf.include?(i).should be_true}
-    unsetbits.each{|i| bf.include?(i).should be_false}
-    (32..99).each{|i| bf.include?(i).should be_false}
+    ((0..31).map{|i| i} - unsetbits).each{|i| bf.include?(i).should be true}
+    unsetbits.each{|i| bf.include?(i).should be false}
+    (32..99).each{|i| bf.include?(i).should be false}
   end
 
   it "should randomly set and unset" do
@@ -56,16 +56,16 @@ describe Bloombroom::BitField do
     other_bits = (0..999).map{|i| i} - random_bits
 
     random_bits.each{|i| bf.set(i)}
-    other_bits.each{|i| bf.include?(i).should be_false}
-    random_bits.each{|i| bf.include?(i).should be_true}
+    other_bits.each{|i| bf.include?(i).should be false}
+    random_bits.each{|i| bf.include?(i).should be true}
 
     other_bits.each{|i| bf.unset(i)}
-    other_bits.each{|i| bf.include?(i).should be_false}
-    random_bits.each{|i| bf.include?(i).should be_true}
+    other_bits.each{|i| bf.include?(i).should be false}
+    random_bits.each{|i| bf.include?(i).should be true}
 
     random_bits.each{|i| bf.unset(i)}
-    other_bits.each{|i| bf.include?(i).should be_false}
-    random_bits.each{|i| bf.include?(i).should be_false}
+    other_bits.each{|i| bf.include?(i).should be false}
+    random_bits.each{|i| bf.include?(i).should be false}
   end
 
   it "should randomly set and unset and support zero?" do
@@ -74,16 +74,16 @@ describe Bloombroom::BitField do
     other_bits = (0..999).map{|i| i} - random_bits
 
     random_bits.each{|i| bf.set(i)}
-    other_bits.each{|i| bf.zero?(i).should be_true}
-    random_bits.each{|i| bf.zero?(i).should be_false}
+    other_bits.each{|i| bf.zero?(i).should be true}
+    random_bits.each{|i| bf.zero?(i).should be false}
 
     other_bits.each{|i| bf.unset(i)}
-    other_bits.each{|i| bf.zero?(i).should be_true}
-    random_bits.each{|i| bf.zero?(i).should be_false}
+    other_bits.each{|i| bf.zero?(i).should be true}
+    random_bits.each{|i| bf.zero?(i).should be false}
 
     random_bits.each{|i| bf.unset(i)}
-    other_bits.each{|i| bf.zero?(i).should be_true}
-    random_bits.each{|i| bf.zero?(i).should be_true}
+    other_bits.each{|i| bf.zero?(i).should be true}
+    random_bits.each{|i| bf.zero?(i).should be true}
   end
 
   it "should report size" do
